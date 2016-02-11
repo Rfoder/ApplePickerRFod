06b615ac257ded43bf8909f906aeb4c001a623b2
Rfoder <bfcat10@yahoo.com>
2016-02-11 01:47:03 -0500

    Up to GUI
    Some problems with the GUITEXT

---
+++ ApplePickerRFod/Assets/AppleTree.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+
+	public GameObject 	applePrefab;
+
+	public float 	speed = 1f;
+
+	public float 	leftAndRightEdge = 10f;
+
+	public float 	chanceToChangeDirections = 0.1f;
+
+	public float 	secondsBetweenAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+		//Drop apples every seconds
+		InvokeRepeating( "DropApple", 2f, secondsBetweenAppleDrops );
+	
+	}
+
+	void DropApple() {
+		GameObject apple = Instantiate ( applePrefab ) as GameObject;
+		apple.transform.position = transform.position;
+	}
+	// Update is called once per frame
+
+	void Update () {
+		//basic movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		//change directions
+
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); // left
+		}
+
+	}
+
+	void FixedUpdate() {
+		//randomly change direction
+		 if (Random.value < chanceToChangeDirections) {
+			speed *= -1; // change direction
+	      }
+		}
+
+    }
+
+	
+	
+
